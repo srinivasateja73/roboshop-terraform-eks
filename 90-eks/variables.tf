8d05bd90914bcdb54e853b8279deb3a98df0ba68
brahmaiah <bramhaiahg@gmail.com>
2025-12-26 15:40:11 +0530

    commit

---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z05699223I80RG51324RM"
+}
+
+variable "domain_name" {
+    default = "dawsteja.store"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
